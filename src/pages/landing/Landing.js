ff5d72ba152f76cefb8ce0e2d1af20e14d892b8d
Mario <canalessanchezmario@gmail.com>
2023-05-07 21:51:46 +0200

    Cambios imports no usados

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
-import landingImage from "../../images/me1.svg";
+// import landingImage from "../../images/me1.svg";
 import Draw from "../../components/Draw";
 import SocialIcons from "../../components/SocialIcons";
 
