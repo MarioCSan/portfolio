a2aaccd58a8209e1f7d637344a556681ed352cc4
Mario <canalessanchezmario@gmail.com>
2023-05-24 23:49:25 +0200

    cambios en las imagenes y app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     availability: "Abierto a escuchar ofertas",
     brand:
       "Creo soluciones y aplicaciones con las últimas tecnologías en el desarrollo web.\n \nMe gusta desarrollar en la plataforma .NET para aprovechar su capacidad multiplataforma con Xamarin/MAUI. \nReact o Angular son mis opciones favoritas para el desarrollo del FrontEnd.\n Además, he creado aplicaciones, que hacen usos de recursos en Azure o AWS.",
-    test: "SIUUUUU",
     presentar:"Dejame Presentarme",
     yoSoy:"Yo soy Mario",
     saludo: "Encantado de conocerte",
