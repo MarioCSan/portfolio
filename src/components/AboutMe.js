2055b49d49c7a3139de629312ea34a63d1676a1c
Mario <canalessanchezmario@gmail.com>
2023-07-18 19:39:59 +0200

    Se modifica AboutMe para añadir las imagenes desde la aplicación en vez de recuperarlas de un servidor. Se usa un json para mostrarlas. se añaden las imagenes también

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,13 +4,14 @@ import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
 import resume from "../pages/about/mario-canales-cv.pdf";
+import projectImages from "../images.json";
+
 
 const AboutMe = ({ name, email, location, availability, brand, pos, saludo }) => {
   const [ref, inView] = useInView({
     threshold: 0.2,
     triggerOnce: true,
   });
-
 
   const [downloading, setDownloading] = useState(false);
 
@@ -28,6 +29,7 @@ const AboutMe = ({ name, email, location, availability, brand, pos, saludo }) =>
       setDownloading(false);
     };
     document.body.appendChild(link);
+
     link.click();
   };
   return (
@@ -80,7 +82,7 @@ const AboutMe = ({ name, email, location, availability, brand, pos, saludo }) =>
               <div className="row">
                 <div className="col-12 col-md-6 info">
                   <span>Email:</span>
-                  <p className='pt-2'>
+                  <p className="pt-2">
                     <a href={`mailto:${email}`}>{email}</a>
                   </p>
                 </div>
@@ -97,91 +99,10 @@ const AboutMe = ({ name, email, location, availability, brand, pos, saludo }) =>
                 <div className="col-12 col-md-12 info pt-2">
                   <span className="mb-4">Conocimientos en:</span>
                   <div className="pt-3 col-12 col-md-12 info">
-                    <img
-                      src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
-                      alt="Javascript"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
-                      alt="React"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
-                      alt="TypeScript"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white"
-                      alt="Angular"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white"
-                      alt="C#"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white"
-                      alt=".NET"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
-                      alt="Next"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/Xamarin-3199DC?style=for-the-badge&logo=xamarin&logoColor=white"
-                      alt="Xamarin"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
-                      alt="HTML5"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
-                      alt="CSS"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white"
-                      alt="Bootstrap"
-                    />
-                    {/* <img src='https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white' alt='Node.js'/> */}
-                    <img
-                      src="https://img.shields.io/badge/azure-%230072C6.svg?style=for-the-badge&logo=azure-devops&logoColor=white"
-                      alt="AZURE"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7"
-                      alt="Netlify"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"
-                      alt="AWS"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white"
-                      alt="npm"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/Microsoft%20SQL%20Sever-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=white"
-                      alt="SQL server"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white"
-                      alt="MySQL"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white"
-                      alt="Postgres"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white"
-                      alt="Mongo"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white"
-                      alt="Docker"
-                    />
-                    <img
-                      src="https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white"
-                      alt="Swagger"
-                    />
+                	  {projectImages.map((lang, i)=>{
+                      return <img src={lang.imagen} alt={lang.title} />;
+                    })}
+
                   </div>
                 </div>
                 {/* <div className="col-12 col-md-6 info">
