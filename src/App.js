a157db3ccc32a4c57f9ec7847420ba0f67ab44f7
Mario <40211718+MarioCSan@users.noreply.github.com>
2023-09-01 22:37:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     name: "Mario Canales",
     location: "Madrid, ES",
     email: "canalessanchezmario@gmail.com",
-    availability: "Abierto a escuchar ofertas",
+    availability: "En busqueda activa de empleo",
     brand: "Creo soluciones y aplicaciones con las últimas tecnologías en el desarrollo web.\n \nMe gusta desarrollar en la plataforma .NET para aprovechar su capacidad multiplataforma con Xamarin/MAUI. \nReact o Angular son mis opciones favoritas para el desarrollo del FrontEnd.\n Además, he creado aplicaciones, que hacen usos de recursos en Azure o AWS.",
     presentar:"Dejame Presentarme",
     yoSoy:"Yo soy Mario",
