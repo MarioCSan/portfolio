296fe912a4e1041d48c57fae9d70a97f4c2bcca3
Mario <canalessanchezmario@gmail.com>
2023-09-02 00:24:45 +0200

    Cambios en las extensiones

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,4 @@
-import aboutMeImg from "../images/aboutme.JPG";
+import aboutMeImg from "../images/aboutme.jpg";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
