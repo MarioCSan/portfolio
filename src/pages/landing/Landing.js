ed7d3eac6b444fa21be601fa1629aef2450443f2
Mario <canalessanchezmario@gmail.com>
2023-09-03 16:16:19 +0200

    Se añaden todos los componentes al landing para poder realizar un scroll más intuitivo

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,6 +1,9 @@
 import Typewriter from "typewriter-effect";
 import SocialIcons from "../../components/SocialIcons";
 import { Link } from "react-router-dom";
+import About from "../about/About";
+import Portfolio from "../portfolio/Portfolio";
+import Contact from "../contact/Contact";
 
 const Landing = ({ name }) => {
 
@@ -41,42 +44,56 @@ const Landing = ({ name }) => {
   };
 
   
-  return(
-    <section className="landing" style={styles.landing}>
-      <div>
-      </div>
-      <div className="textContainer" style={styles.textContainer}>
-        <h1 className="name" style={styles.name}>
-          {name}
-        </h1>
-        <div className="description  mb-4">
-          <Typewriter
-            className="description"
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("Desarrollador Backend")
-                .pauseFor(1500)
-                .deleteChars(7)
-                .pauseFor(500)
-                .typeString("Frontend")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("Traigo tus ideas a la vida")
-                .start();
-            }}
-          />
-        </div>
-        <div className="m-4 m p-6 btncontainer">
-          <Link to="/sobremi">
-            <button className="btn m-4 downloadCV button">Conóceme</button>
-          </Link>
-          <Link to="/portfolio">
-            <button className="btn m-4 downloadCV button">Portfolio</button>
-          </Link>
-        </div>
-      </div>
-      <SocialIcons />
-    </section>
+  return (
+    <>
+      <>
+        {/* Main Landing Page */}
+      
+          {/* Display the drawing component */}
+          <section className="landing" style={styles.landing}>
+            <div></div>
+            <div className="textContainer" style={styles.textContainer}>
+              <h1 className="name" style={styles.name}>
+                {name}
+              </h1>
+              <div className="description  mb-4">
+                <Typewriter
+                  className="description"
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Desarrollador Backend")
+                      .pauseFor(1500)
+                      .deleteChars(7)
+                      .pauseFor(500)
+                      .typeString("Frontend")
+                      .pauseFor(1500)
+                      .deleteAll()
+                      .typeString("Traigo tus ideas a la vida")
+                      .start();
+                  }}
+                />
+              </div>
+              <div className="m-4 m p-6 btncontainer">
+                <Link to="/sobremi">
+                  <button className="btn m-4 downloadCV button">
+                    Conóceme
+                  </button>
+                </Link>
+                <Link to="/portfolio">
+                  <button className="btn m-4 downloadCV button">
+                    Portfolio
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <SocialIcons />
+          </section>
+        {/* Display the about section */}
+        <About />
+        <Portfolio />
+        <Contact />
+      </>
+    </>
   );
 };
 
