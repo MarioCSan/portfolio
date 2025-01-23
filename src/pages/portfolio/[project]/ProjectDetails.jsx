04f3f10062e1b06d5b82eec620b4373401176327
Mario <canalessanchezmario@gmail.com>
2025-01-23 16:25:26 +0100

    Minor fixes

--- src/pages/portfolio/[project]/ProjectDetails.jsx
+++ src/pages/portfolio/[project]/ProjectDetails.jsx
@@ -87,7 +87,7 @@ const ProjectDetails = () => {
                 <a href={project.github}>
                   <Button name="Ver código" />
                 </a>
-                {project.disabled == false &&
+                {project.disabled === false &&
                 <a href={project.deployed}>
                   <Button name="Ver demo" />
                 </a>
